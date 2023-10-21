792c3eb4743ddd81dec4ad8239d1aa13a7848e16
birdman093 <russfeathers@gmail.com>
2023-10-21 12:45:44 -0400

    Next.js Template

---
+++ air-ball/components/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <div>
+            <Link href="/">HOME</Link>
+            <Link href="/test">TEST</Link>
+        </div>
+    )
+
+}
---
+++ air-ball/pages/index.tsx
@@ -0,0 +1,6 @@
+import Header from "@/components/header"
+
+export default function Home() {
+    return (<Header></Header>)
+
+}
